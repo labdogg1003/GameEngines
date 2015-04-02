7b9390fac76e0acd7e9bdfa79b83758e3bd2c1be
shadedpast <the21192@gmail.com>
2015-04-02 16:17:03 -0500

    Switch Room(2)
    Added Switch Room, open door/spawn enemies.
    Enemies don't die and go through player.

---
+++ Assets/EnemySwitch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySwitch : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public GameObject enemy;
+    public int once = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (once == 0)
+        {
+            enemy.SetActive(true);
+            once++;
+        }
+    }
+}
