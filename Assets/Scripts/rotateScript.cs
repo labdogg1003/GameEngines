57966dfa64bcdd5985e91777b0c3fd8a0eded9b4
schadman3 <brandon@Brandons-MacBook-Pro.local>
2015-04-03 19:04:20 -0500

    Added Features

---
+++ Assets/Scripts/rotateScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateScript : MonoBehaviour {
+
+	public float xRotation = 90.0f;
+	public float xRotation1 = 0.0f;
+	public float RotationSpeed = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate ( Vector3.up * ( RotationSpeed * Time.deltaTime ) );
+	}
+}
