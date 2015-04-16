88db33b3bd0f934dd60a8f47ef647d9c63df66cd
shadedpast <the21192@gmail.com>
2015-04-16 17:45:52 -0500

    Revert "damage attempt"
    This reverts commit bd3362cd84383baf9cc03879e2dc70c6af1e25d1.

--- Assets/UIHealth.cs
+++ Assets/UIHealth.cs
@@ -7,28 +7,9 @@ public class UIHealth : MonoBehaviour {
 	void Start () {
 	
 	}
-
-    public GameObject player;
-    public GameObject heart1;
-    public GameObject heart2;
-    public GameObject heart3;
-
+	
 	// Update is called once per frame
 	void Update () {
-        PlayerHealth playerHealth = GetComponent<PlayerHealth>();
-        if(playerHealth.currentHealth == 2){
-            heart3.active = false;
-        }
-        if (playerHealth.currentHealth == 1)
-        {
-            heart3.active = false;
-            heart2.active = false;
-        }
-        if (playerHealth.currentHealth == 0)
-        {
-            heart3.active = false;
-            heart2.active = false;
-            heart1.active = false;
-        }
+	
 	}
 }
