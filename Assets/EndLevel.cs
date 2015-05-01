9741ff0655154fd1c344da59ca7cecb669746cd5
labdogg1003 <labguitar1003@gmail.com>
2015-04-30 20:09:58 -0500

    Added end of level

---
+++ Assets/EndLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel: MonoBehaviour 
+{
+	void ReloadLevel()
+	{
+		Application.LoadLevel("Level_1");
+	}
+
+	void OnTriggerEnter()
+	{
+		ReloadLevel();
+	}
+}
