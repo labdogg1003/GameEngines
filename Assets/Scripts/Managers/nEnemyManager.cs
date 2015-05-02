058240fbc1c4c101509294138e458dcbb42f6231
shadedpast <the21192@gmail.com>
2015-05-01 22:57:38 -0500

    bug fix
    some bug fixes with enemy spawns

--- Assets/Scripts/Managers/nEnemyManager.cs
+++ Assets/Scripts/Managers/nEnemyManager.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 
-public class EnemyManager : MonoBehaviour
+public class nEnemyManager : MonoBehaviour
 {
     public GameObject[] enemy;
 	public GameObject   poof;
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
+
+    public int spawned = 0;
+    public int spawns = 0;
 
 	private Animator anim;                      // Reference to the animator component.
 	public GameObject target;                   // The Door that the trigger will open
@@ -23,10 +26,12 @@ public class EnemyManager : MonoBehaviour
     {
 		int enemyIndex      = Random.Range (0, enemy.Length);
         int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-
-        Instantiate(poof, spawnPoints[spawnPointIndex].position, Quaternion.identity);
-        (Instantiate(enemy[enemyIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation) as GameObject).transform.parent = transform.parent;
-     
+        if (spawned < spawns)
+        {
+            Instantiate(poof, spawnPoints[spawnPointIndex].position, Quaternion.identity);
+            (Instantiate(enemy[enemyIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation) as GameObject).transform.parent = transform.parent;
+            spawned++;
+        } 
     }
 
 	void OnTriggerEnter()
